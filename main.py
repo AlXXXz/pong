346578e67846fe63d53401deaed058d62bcd567c
AlXXXz <86950837+AlXXXz@users.noreply.github.com>
2023-09-19 16:53:26 +0400

    Update main.py
    Добавлен: отскок мяча, голы, подсчёт голов

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 
 WIDTH = 800
 HEIGHT = 600
-FPS = 30
+FPS = 300
 
 # Задаем цвета
 WHITE = (255, 255, 255)
@@ -13,6 +13,9 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 153)
 
+PlayerWidth = 20
+PlayerHeight = 60
+
 # Создаем игру и окно
 pygame.init()
 pygame.mixer.init()
@@ -20,124 +23,90 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("My Game")
 clock = pygame.time.Clock()
 
+PlayerScore = 0
+Player2Score = 0
+
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((50, 50))
+        self.image = pygame.Surface((PlayerWidth, PlayerHeight))
         self.image.fill(GREEN)
         self.rect = self.image.get_rect()
         self.rect.center = (WIDTH / 6, HEIGHT / 2)
         self.speedy = 0
+        self.score = 0
 
     def update(self):
         self.speedy = 0
         keystate = pygame.key.get_pressed()
         if keystate[pygame.K_w]:
-            self.speedy = -8
+            self.speedy = -2
         if keystate[pygame.K_s]:
-            self.speedy = 8
+            self.speedy = 2
         if self.rect.y > 0 or self.rect.y < (HEIGHT - 60):
             self.rect.y += self.speedy
+        
 
 class Player2(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((50, 50))
+        self.image = pygame.Surface((PlayerWidth, PlayerHeight))
         self.image.fill(YELLOW)
         self.rect = self.image.get_rect()
         self.rect.center = (WIDTH - (WIDTH / 6), HEIGHT / 2)
         self.speedy = 0
+        self.score = 0
 
     def update(self):
         self.speedy = 0
         keystate = pygame.key.get_pressed()
         if keystate[pygame.K_UP]:
-            self.speedy = -8
+            self.speedy = -2
         if keystate[pygame.K_DOWN]:
-            self.speedy = 8
+            self.speedy = 2
         if self.rect.y > 0 or self.rect.y < (HEIGHT - 60):
             self.rect.y += self.speedy
-        #print(self.rect.y)
 
-# class Ball(pygame.sprite.Sprite):
-#     def __init__(self):
-#         pygame.sprite.Sprite.__init__(self)
-#         self.image = pygame.Surface((15, 15))
-#         self.image.fill(RED)
-#         self.rect = self.image.get_rect()
-#         self.rect.center = (WIDTH / 2, HEIGHT / 2)
-#     def update(self):
-#         self.rect.x += 5
-#         print(self.rect.x)
-#         if self.rect.left >= WIDTH:
-#             self.rect.x *= -1
-
-class Ball:
-    def __init__(self, posx, posy, radius, speed, color):
-        self.posx = posx
-        self.posy = posy
-        self.radius = radius
-        self.speed = speed
-        self.color = color
+class Ball(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((15, 15))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
         self.xFac = 1
-        self.yFac = -1
-        self.ball = pygame.draw.circle(
-            screen, self.color, (self.posx, self.posy), self.radius)
-        self.firstTime = 1
+        self.yFac = 1
 
-    def display(self):
-        self.ball = pygame.draw.circle(
-            screen, self.color, (self.posx, self.posy), self.radius)
- 
     def update(self):
-        self.posx += self.speed*self.xFac
-        self.posy += self.speed*self.yFac
- 
-        # If the ball hits the top or bottom surfaces,
-        # then the sign of yFac is changed and it
-        # results in a reflection
-        if self.posy <= 0 or self.posy >= HEIGHT:
+        self.speedx = 1
+        self.speedy = 1
+        # self.speedy = 5
+        self.rect.x += self.speedx * self.xFac
+        self.rect.y += self.speedy * self.yFac
+        # if self.rect.x >= WIDTH or self.rect.x <= 0:
+        #     self.xFac *= -1
+        if self.rect.y >= HEIGHT or self.rect.y <= 0:
             self.yFac *= -1
- 
-        # If the ball touches the left wall for the first time,
-        # The firstTime is set to 0 and we return 1
-        # indicating that Geek2 has scored
-        # firstTime is set to 0 so that the condition is
-        # met only once and we can avoid giving multiple
-        # points to the player
-        if self.posx <= 0 and self.firstTime:
-            self.firstTime = 0
-            return 1
-        elif self.posx >= WIDTH and self.firstTime:
-            self.firstTime = 0
-            return -1
-        else:
-            return 0
- 
-    # Used to reset the position of the ball
-    # to the center of the screen
-    def reset(self):
-        self.posx = WIDTH /2
-        self.posy = HEIGHT /2
-        self.xFac *= -1
-        self.firstTime = 1
- 
-    # Used to reflect the ball along the X-axis
-    def hit(self):
-        self.xFac *= -1
- 
-    def getRect(self):
-        return self.ball
+
 
 all_sprites = pygame.sprite.Group()
 player = Player()
 player2 = Player2()
-# ball = Ball()
+ball = Ball()
 all_sprites.add(player)
 all_sprites.add(player2)
-ball = Ball(WIDTH / 2, HEIGHT / 2, 7, 7, RED)
-# all_sprites.add(ball)
+all_sprites.add(ball)
 
+# def resetBall():
+#     ball.rect.center = (WIDTH / 2, HEIGHT / 2)
+
+def resetBallPlayer():
+    ball.rect.center = (player.rect.x + 30, player.rect.y + (PlayerHeight / 2)) #((WIDTH / 6) + 20, HEIGHT / 2)
+    player.score += 1
+
+def resetBallPlayer2():
+    ball.rect.center = (player2.rect.x - 30, player2.rect.y + (PlayerHeight / 2))
+    player2.score += 1
 # Цикл игры
 running = True
 while running:
@@ -157,13 +126,24 @@ while running:
     # Обновление
     all_sprites.update()
     
-    
+
+    if ball.rect.x == player.rect.x + PlayerWidth and (ball.rect.y >= player.rect.y - 50 and ball.rect.y <= player.rect.y + 50):
+        ball.xFac *= -1
+    if ball.rect.x >= player2.rect.x - PlayerWidth and (ball.rect.y >= player2.rect.y - 50 and ball.rect.y <= player2.rect.y + 50):
+        ball.xFac *= -1
+
+    if ball.rect.x >= WIDTH:
+        resetBallPlayer()
+    if ball.rect.x <= 0:
+        resetBallPlayer2()
+
+    print(player.score, ":", player2.score)
+
+    #print("ball X Y", ball.rect.x, ball.rect.y, "Pos X Y:", player2.rect.x, player2.rect.y, "top:", player2.rect.y, "mid:", player2.rect.y + (PlayerHeight / 3), "bottom", player2.rect.y + (2 * (PlayerHeight / 3)))
+
+
     # Рендеринг
     screen.fill(BLACK)
-    point = ball.update()
-    if point:  
-        ball.reset()
-    ball.display()
     pygame.draw.line(screen, WHITE, (WIDTH / 2, 0), (WIDTH / 2, HEIGHT), 7)
     all_sprites.draw(screen)
     # После отрисовки всего, переворачиваем экран
